59d904846e6764c6fc154edfd81ef77195b49859
nathanRomad <nathan.a.hamilton@gmail.com>
2021-06-23 13:32:53 -0500

    fix: added bar chart to analytics page

--- src/components/scoresheet/AnalyticsList.js
+++ src/components/scoresheet/AnalyticsList.js
@@ -1,26 +1,30 @@
 import React, { useContext, useEffect, useState } from "react"
 import { AnalyticsContext } from "./AnalyticsProvider"
-import { MyResponsivePie } from "./ScorePie"
+import { MyResponsiveBar } from "./BarChart"
 
 export const Analytics = (props) => {
     const { getSummary, getAnalytics } = useContext(AnalyticsContext)
     const [summary, setSummary] = useState({})
     const [analytics, setAnalytics] = useState({})
     const [data, setData] = useState([])
-
+    
     useEffect(() => {
         getSummary()
-            .then((res) => {
-                setSummary(res)
-            })
+        .then((res) => {
+            setSummary(res)
+        })
     }, [])
-
+    
     useEffect(() => {
         getAnalytics()
-            .then((res) => {
-                setAnalytics(res)
-            })
+        .then((res) => {
+            setAnalytics(res)
+        })
     }, [])
+    
+    useEffect(() => {
+        setData(analytics.data)
+    }, [analytics])
     
 
     return (
@@ -29,7 +33,8 @@ export const Analytics = (props) => {
             <h2>Analysis One</h2>
             <div>Total Liabilities: {analytics.detailedAnalysisOne?.totalLiabilities}</div>
             <div>Retirement Savings: {analytics.detailedAnalysisOne?.retirementSavings}</div>
+            <div style={{width: 450, height: 450 }}> {data?.length ? <MyResponsiveBar data={data}/>: <></>}</div>
             <br></br>
-        </>
+     </>
     )
 }
